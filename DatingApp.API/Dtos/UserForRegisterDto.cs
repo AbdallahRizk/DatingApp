9ddeb9d540c3497753fc4657958d7b086012b990
Abdallah Rizk <abm.rizk@gmail.com>
2020-10-27 16:50:29 +0300

    loginMethod on auth controller

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-        public string Username { get; set; }    
+        [Required]
+        public string Username { get; set; }   
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage="You must speicfy password betwwen 4 and 8")] 
         public string Password { get; set; }
     }
 }
